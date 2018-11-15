c1ed5012c3131616f09d05c3996203a601d8e894
Lyn Al Masri <lynmasry@gmail.com>
2018-11-15 21:37:54 +0300

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Lyn Almasri on 11/15/18.
+//  Copyright © 2018 lynmasri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
