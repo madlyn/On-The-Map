d575e668c8187ee6a037a48e4b00cf7292b26c38
Lyn Al Masri <lynmasry@gmail.com>
2018-12-15 20:23:18 +0300

    update url

--- On The Map/Model/Constants.swift
+++ On The Map/Model/Constants.swift
@@ -17,8 +17,8 @@ class Constants{
     static let ParseApplicationId = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
     static let RESTAPIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
     // MARK: Udacity
-    static let UdacityApiHost = "www.udacity.com"
-    static let UdacityApiPath = "/api"
+    static let UdacityApiHost = "onthemap-api.udacity.com"
+    static let UdacityApiPath = "/v1"
     
     struct APIMethods {
         // MARK: Parse
